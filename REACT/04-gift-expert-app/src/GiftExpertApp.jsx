22297a656e4db5e47b1a20243fb48b6eadf66f77
n_narcise <joaquin.narcisse@gmail.com>
2022-12-21 18:26:45 -0500

    test

--- REACT/04-gift-expert-app/src/GiftExpertApp.jsx
+++ REACT/04-gift-expert-app/src/GiftExpertApp.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import { AddCategory } from './components/AddCategory'
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GiftExpertApp = () => {
-    const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball' ])
-    console.log(categories)
-    const onAddCategory = () => {
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+  // console.log(categories);
+  const onAddCategory = () => {/* esta funcion es para agregar lo que se me antoje a travez del boton de agregar */
     //    console.log('Kimetsu no yaiba')
-       //setCategories([ ...categories, 'Kimetsu no yaiba' ])
-       setCategories( cat =>[ ...cat, 'Kimetsu no yaiba' ])
-       /* cualquiera de las dos opciones son validas */
-    }
+    //setCategories([ ...categories, 'Kimetsu no yaiba' ])
+    setCategories((cat) => [...cat, "Kimetsu no yaiba"]);
+    /* cualquiera de las dos opciones son validas */
+  };
+
   return (
     <>
-    {/* titulo */}
-    <h1>GiftExpertApp</h1>
+      {/* titulo */}
+      <h1>GiftExpertApp</h1>
 
-    {/* input */}
-    <AddCategory />
-    <button onClick={onAddCategory} >Agregar</button>
+      {/* input */}
+      <AddCategory 
+      //setCategories={setCategories} 
+      // setCategories={setCategories}
+        /* setCategories={setCategories} es una metodologia que oculta 
+      loque realmente esta sucediendo por lo tanto no es evidente su lectura */
+      onNewCategory={(event) => onAddCategory(event)} // el prefijo on significa que se esta emitiendo algo es decir, un evento
+      /* en el siguinete codigo: onNewCategory={ ( event ) => onNewCategory(event) } podemos por ley eliminar la sitxis del event 
+       ya que estan en ambos lados de la funcion. */
+      /* RECURDA QUE EL 'onNewCategory' ES UNA PROPIEDAD DE MI COMPONENTE*/
+      />
+      {/*     <button onClick={onAddCategory} >Agregar</button>
+       */}
 
-    {/* listado de gifts */}
-    <ol>
-        {
-            categories.map(category =>{
-                return <li key={category}>{category}</li>
 
-            }  )
-        }
-    </ol>
+      {/* listado de gifts */}
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
-  )
-}
+  );
+};
--- REACT/04-gift-expert-app/src/components/AddCategory.jsx
+++ REACT/04-gift-expert-app/src/components/AddCategory.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 
-export const AddCategory = () => {
+export const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState("");
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim().length <= 1) return;
+    setCategories((categories) => [inputValue, ...categories]);
+    setInputValue("");
+  };
+
+
   return (
-    <input 
-      type = 'text'
-      placeholder="Buscar Gifts"
-    />
-  )
-}
+    <form onSubmit={(event) => onSubmit(event)}>
+      <input
+        type="text"
+        placeholder="Buscar Gifts"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
