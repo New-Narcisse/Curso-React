bfdf98d2c10cfa4ad93f6023c40bce4dc2c5fb86
n_narcise <joaquin.narcisse@gmail.com>
2022-12-21 20:05:44 -0500

    testing

--- REACT/04-gift-expert-app/src/GiftExpertApp.jsx
+++ REACT/04-gift-expert-app/src/GiftExpertApp.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GiftGrid } from "./components/GiftGrid";
 
 export const GiftExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
   // console.log(categories);
-  const onAddCategory = () => {/* esta funcion es para agregar lo que se me antoje a travez del boton de agregar */
+  const onAddCategory = (newCategory) => {
+    /* esta funcion es para agregar lo que se me antoje a travez del boton de agregar o enter*/
+    if (categories.includes(newCategory)) return;
     //    console.log('Kimetsu no yaiba')
-    //setCategories([ ...categories, 'Kimetsu no yaiba' ])
-    setCategories((cat) => [...cat, "Kimetsu no yaiba"]);
+    setCategories([newCategory, ...categories]);
+    //setCategories((cat) => [...cat, newCategory]);
     /* cualquiera de las dos opciones son validas */
   };
 
@@ -17,27 +20,36 @@ export const GiftExpertApp = () => {
       <h1>GiftExpertApp</h1>
 
       {/* input */}
-      <AddCategory 
-      //setCategories={setCategories} 
-      // setCategories={setCategories}
+      <AddCategory
+        //setCategories={setCategories}
+        // setCategories={setCategories}
         /* setCategories={setCategories} es una metodologia que oculta 
       loque realmente esta sucediendo por lo tanto no es evidente su lectura */
-      onNewCategory={(event) => onAddCategory(event)} // el prefijo on significa que se esta emitiendo algo es decir, un evento
-      /* en el siguinete codigo: onNewCategory={ ( event ) => onNewCategory(event) } podemos por ley eliminar la sitxis del event 
+        onNewCategory={(event) => onAddCategory(event)} // el prefijo on significa que se esta emitiendo algo es decir, un evento
+        /* en el siguinete codigo: onNewCategory={ ( event ) => onNewCategory(event) } podemos por ley eliminar la sitxis del event 
        ya que estan en ambos lados de la funcion. */
-      /* RECURDA QUE EL 'onNewCategory' ES UNA PROPIEDAD DE MI COMPONENTE*/
+        /* RECURDA QUE EL 'onNewCategory' ES UNA PROPIEDAD DE MI COMPONENTE*/
       />
       {/*     <button onClick={onAddCategory} >Agregar</button>
        */}
 
-
       {/* listado de gifts */}
 
       <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
+        {categories.map((category) => (
+          <GiftGrid 
+          key={category}
+          category = { category }
+          />
+        ))}
       </ol>
     </>
   );
 };
+
+{
+  /* <div key={category}>
+  <h3> {category} </h3>
+  <li>{category}</li>
+</div> */
+}
