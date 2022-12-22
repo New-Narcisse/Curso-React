8405f56b86c45da02dee8c79c758021302cf05f5
n_narcise <joaquin.narcisse@gmail.com>
2022-12-22 11:51:55 -0500

    http-server

--- REACT/04-gift-expert-app/src/components/GiftGrid.jsx
+++ REACT/04-gift-expert-app/src/components/GiftGrid.jsx
@@ -1,16 +1,23 @@
+
+import { useEffect, useState } from "react";
+import { getGifts } from "../Helpers/GetGifts";
+
 export const GiftGrid = ({ category }) => {
-  const getGifts = async () => {
-    const url = `api.giphy.com/v1/gifs/search?api_key=xSz35fny2Uxmw0Dye9acBeSzRg3BmWH0&q=${category}`;
-    const resp = await fetch(url);
-    const { data } = await resp.json();
 
-    console.log(data);
-  };
+  const [counter, setCounter] = useState(0)
+  useEffect( () =>{       //RECUERDA QUE EL PREFIJO 'use' SIGNIFICA QUE ES UN HOOK!!!
+    getGifts(category);
 
-  getGifts();
+  }, [ ]) // el segundo argumento es una dependencia. Si se deja vacio significa solo cargara el hook una sola vez 
+  // es decir que la funcion 'getGifts' solo se ejecutara una ves al ejecutar el hook 
+  //esto es para evitar que se ejecute el getGifts por cada alteracion de la app
+  
   return (
     <>
       <h3> {category} </h3>
+      <h5> { counter} </h5>
+      <button onClick={() => setCounter( counter + 1 )}>+1</button>
+
     </>
   );
 };
