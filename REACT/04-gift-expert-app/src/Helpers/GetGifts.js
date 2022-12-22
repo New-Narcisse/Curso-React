935bbaa73d590cfffd98eddc5a96b61190f73952
n_narcise <joaquin.narcisse@gmail.com>
2022-12-22 13:49:38 -0500

    Showing imgs

--- REACT/04-gift-expert-app/src/Helpers/GetGifts.js
+++ REACT/04-gift-expert-app/src/Helpers/GetGifts.js
@@ -1,5 +1,5 @@
 export const getGifts = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=xSz35fny2Uxmw0Dye9acBeSzRg3BmWH0&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=xSz35fny2Uxmw0Dye9acBeSzRg3BmWH0&q=${category}&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifts = data.map(img => ( {
@@ -8,6 +8,5 @@ export const getGifts = async (category) => {
       url: img.images.downsized_medium.url
     } ) )
   
-    console.log(gifts);
     return gifts;
   };
--- REACT/04-gift-expert-app/src/components/GiftGrid.jsx
+++ REACT/04-gift-expert-app/src/components/GiftGrid.jsx
@@ -1,23 +1,40 @@
-
 import { useEffect, useState } from "react";
 import { getGifts } from "../Helpers/GetGifts";
+import { GiftItem } from "./GiftItem";
 
 export const GiftGrid = ({ category }) => {
-
-  const [counter, setCounter] = useState(0)
-  useEffect( () =>{       //RECUERDA QUE EL PREFIJO 'use' SIGNIFICA QUE ES UN HOOK!!!
-    getGifts(category);
-
-  }, [ ]) // el segundo argumento es una dependencia. Si se deja vacio significa solo cargara el hook una sola vez 
-  // es decir que la funcion 'getGifts' solo se ejecutara una ves al ejecutar el hook 
+  const [images, setImages] = useState([]); // los [] es para inicializar nuestro hook con arreglo vacio, se puede hacer tambien con
+  const getImages = async () => {
+    const newImages = await getGifts({ category });
+    setImages(newImages);
+  };
+  useEffect(() => {
+    //RECUERDA QUE EL PREFIJO 'use' SIGNIFICA QUE ES UN HOOK!!!
+    getImages();
+  }, []); // el segundo argumento es una dependencia. Si se deja vacio significa solo cargara el hook una sola vez
+  // es decir que la funcion 'getGifts' solo se ejecutara una ves al ejecutar el hook
   //esto es para evitar que se ejecute el getGifts por cada alteracion de la app
-  
+
   return (
     <>
       <h3> {category} </h3>
-      <h5> { counter} </h5>
-      <button onClick={() => setCounter( counter + 1 )}>+1</button>
-
+      <div className="card-grid">
+        {images.map(
+          (
+            image //esto es para mostrar lo que yo quiera. por eso creamos el hook useState
+          ) => (
+            <GiftItem
+              key={image.id}
+              {...image}
+            /> /* este componente reemplaza el codigo {images.map......} por cuestiones de buna practica */
+            /* el img del .map puede ser cualquier otro nombre o abreviacion que se quiera.
+                { images.map( ( img ) => (
+            <li key={ img.id } >{ img.title }</li> tooooodo este codigo se puede mejorar 
+            desestructurando las variables que necesito justo despues del .map y queda asi: { images.map( ({ id, title }) => (
+            <li key={ id } >{ title }</li> */
+          )
+        )}
+      </div>
     </>
   );
 };
