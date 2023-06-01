d7e0b616d85802b866254635f05353eea303b7cc
n_narcise <joaquin.narcisse@gmail.com>
2023-06-01 11:37:11 -0500

    Adding

--- REACT/09-toolkit-redux/src/store/apis/todosApi.js
+++ REACT/09-toolkit-redux/src/store/apis/todosApi.js
@@ -1,10 +1,23 @@
-import {createApi} from '@reduxjs/toolkit/query/react'
+import {createApi,fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
 
 export const todosApi = createApi({
+    reducerPath: 'todos',
+    baseQuery: fetchBaseQuery({
+        baseUrl: ' https://jsonplaceholder.typicode.com ',
+
+    }),
+    endpoints: (builder) => ({
+        getTodos: builder.query({
+            query: () => '/todos'
+        })
+    })
+
 
 })
 
 
-export const { } = todosApi;
+export const { useGetTodosQuery } = todosApi;
+
+
